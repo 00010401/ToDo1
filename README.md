8e9e3beaf899554fbaa8ffa14d10c2e326d29652
00010401 <stud00010401@gmail.com>
2021-04-01 21:05:48 +0500

    readme file added

---
+++ README.md
@@ -0,0 +1,28 @@
+# TO DO WEB APPLICATION
+
+### This web application helps users to create, delete and review tasks.
+
+To run this web application locally, you should follow the steps below:
+
+1. Clone this repository
+```bash
+git clone https://github.com/00010401/ToDo1
+```
+
+2. Install dependencies of this project
+```bash
+cd [project folder]
+npm install [dependencies]
+```
+
+3. Run the application
+```bash
+node app
+```
+
+### Web application dependencies
+- express.js
+- pug.js
+
+### Web application repository on github
+[Link to github repository](https://github.com/00010401/ToDo1)
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs')
+
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded( { extended: false }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -13,9 +16,77 @@ app.get('/create', (req, res) => {
     res.render('create')
 })
 
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+    const date = req.body.date
+
+    if (title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true})
+    }
+    else{
+        fs.readFile('./data/tasks.json', (err, data) => {
+            if (err) throw err
+
+            const tasks = JSON.parse(data)
+
+            tasks.push({
+                id: id (),
+                title: title,
+                description: description,
+                date: date
+            })
+
+            fs.writeFile('./data/tasks.json', JSON.stringify(tasks), err =>{
+                if (err) throw err
+
+            res.render('create', { success: true })
+            })
+        })
+    }
+})
+
+app.get('/api/v1/tasks', (req, res) => {
+    fs.readFile('./data/tasks.json', (err, data) => {
+        if (err) throw err
+
+        const tasks = JSON.parse(data)
+
+        res.json(tasks)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+
+    fs.readFile('./data/tasks.json', (err, data) => {
+        if (err) throw err
+
+        const tasks = JSON.parse(data)
+
+        res.render('tasks', { tasks: tasks })
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/tasks.json', (err, data) => {
+        if (err) throw err
+
+        const tasks = JSON.parse(data)
+
+        const task = tasks.filter(task => task.id == id)[0]
+
+        res.render('detail', { task: task })
+    })
+})
 
 app.listen(8000, err => {
     if (err) console.log(err)
 
     console.log('Server is running on port 8000...')
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9)
+}
